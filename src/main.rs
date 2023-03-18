821736e3fe186243054d63f9f2fce61205b95995
jkrueger721 <johnathankrueger@me.com>
2023-03-18 11:01:23 -0400

    add ability to take cli args

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,22 @@
 use std::collections::HashMap;
+use std::env;
 use std::fs::File;
 use std::io::BufReader;
+use std::io::Read;
+use std::path::Path;
 
 fn main() {
-    file_read();
+    let args: Vec<String> = env::args().collect();
+    file_read(&args[1]);
 }
 
-fn file_read() {
-    let file = File::open("test.txt").expect("no file in path");
+fn file_read(path: &String) {
+    assert!(Path::new(&path).exists());
+    let file = File::open(path).expect("no file in path");
     let mut reader = BufReader::new(file);
     let mut file_contents = String::new();
+
+    reader.read_to_string(&mut file_contents);
     let collect = file_contents.split_whitespace().collect::<Vec<&str>>();
 
     let mut word_counter: HashMap<&str, i32> = HashMap::new();
